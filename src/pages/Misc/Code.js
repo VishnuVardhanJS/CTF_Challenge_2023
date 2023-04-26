a1c7783b67e296cff2027fc88b33eb9fbf8788d0
VishnuVardhanJS <jaisrini76@gmail.com>
2023-04-26 09:21:45 +0530

    urls updated

--- src/pages/Misc/Code.js
+++ src/pages/Misc/Code.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DisplayCard from '../../components/DisplayCard/DisplayCard'
 import '../Main/Main.css'
+import { collection, onSnapshot } from "firebase/firestore";
+import db from "../../firebase/firebase_config";
 
 function Code() {
 
-  // const [Round1, setRound1] = useState("");
-  // const [Round2, setRound2] = useState("");
+  const [Round1, setRound1] = useState("");
+  const [Round2, setRound2] = useState("");
+
+
+  useEffect(() => {
+    onSnapshot(collection(db, "code"), (snapshot) => {
+      const code = snapshot.docs.map((doc) => doc.data())[0];
+      setRound1(code["Round1"]);
+      setRound2(code["Round2"]);
+      
+    });
+  });
 
   const data = [
     {
         title: "Round 1",
-        url: "/"
+        url: Round1
     },
     {
         title: "Round 2",
-        url: "https://docs.google.com/forms/d/1cuNJ-KyhIRGJuNZdQXcPuPdfqjhKzRLlhqc5Lxmi5qA/viewform?ts=6447aeb8&edit_requested=true"
+        url: Round2
     },
 ]
 
